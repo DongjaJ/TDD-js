172d1b68d227caf7685b3b18ece06769ffd23f90
DongjaJ <jdh981118@naver.com>
2024-07-01 19:42:42 +0900

    test: p43 실패 테스트가 작동할 수 있게 dallor 클래스 추가

---
+++ src/js/dollar.js
@@ -0,0 +1,7 @@
+export default class Dallor {
+  amount;
+
+  constructor(amount) {}
+
+  times(multiplier) {}
+}
--- src/js/money.spec.js
+++ src/js/money.spec.js
@@ -1,7 +1,8 @@
 import { describe, expect } from "vitest";
+import Dallor from "./dollar.js";
 
 describe("money Test", () => {
-  const five = new Five(5);
+  const five = new Dallor(5);
   five.times(2);
   expect(five.amount).toBe(10);
 });
