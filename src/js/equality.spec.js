3a0a1d7ee0f206b26b9c7e6ba447d5f2cb619729
DongjaJ <jdh981118@naver.com>
2024-07-02 20:35:43 +0900

    test(8): 테스트 코드 팩토리 함수로 변경

--- src/js/equality.spec.js
+++ src/js/equality.spec.js
@@ -1,19 +1,19 @@
-import { Franc, Dollar } from "./money.js";
+import { Money } from "./money.js";
 
 describe("equality Test", () => {
   it("같은 값을 생성자에 넣은 dallor 객체 테스트 ", () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
   });
 
   it("다른 값을 생성자에 넣은 dallor 객체 테스트 ", () => {
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
   });
 
   it("같은 값을 생성자에 넣은 Franc 객체 테스트 ", () => {
-    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
+    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
   });
 
   it("다른 값을 생성자에 넣은 Franc 객체 테스트 ", () => {
-    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
   });
 });
--- src/js/money.spec.js
+++ src/js/money.spec.js
@@ -1,10 +1,10 @@
-import { Dollar, Money } from "./money.js";
+import { Money } from "./money.js";
 
 describe("Franc Test", () => {
   it("Money 팩토리 함수", () => {
     const five = Money.dollar(5);
 
-    expect(five.times(2)).toEqual(new Dollar(10));
-    expect(five.times(3)).toEqual(new Dollar(15));
+    expect(five.times(2)).toEqual(Money.dollar(10));
+    expect(five.times(3)).toEqual(Money.dollar(15));
   });
 });
