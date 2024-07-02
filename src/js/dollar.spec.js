80af5b33f2afa05c10f52420b53f6510a052a8cb
DongjaJ <jdh981118@naver.com>
2024-07-02 19:26:20 +0900

    fix: 추후 추가할 테스트 지금은 삭제

--- src/js/dollar.spec.js
+++ src/js/dollar.spec.js
@@ -2,22 +2,17 @@ import { describe, expect, it } from "vitest";
 import Dallor from "./dollar.js";
 
 describe("dallor Test", () => {
-  it.todo("$5+ 10CHF=$10 (환율이 2:1일 경우)");
-
   it("$5 * 2 = $10", () => {
     const five = new Dallor(5);
 
     expect(five.times(2)).toEqual(new Dallor(10));
   });
 
-  it.todo("amount를 private로 만들기");
   it("dollar 부작용 : 연산을 수행한 후에 dollar의 값이 바뀌지 않는다", () => {
     const five = new Dallor(5);
     expect(five.times(2)).toEqual(new Dallor(10));
     expect(five.times(3)).toEqual(new Dallor(15));
   });
-
-  it.todo("money 반올림");
 
   describe("equals() ?", () => {
     it("dallar 5 === dallar 5", () => {
@@ -27,10 +22,4 @@ describe("dallor Test", () => {
       expect(new Dallor(5).equals(new Dallor(6))).toBeFalsy();
     });
   });
-
-  it.todo("hashCode() ?");
-
-  it.todo("equal null() ?");
-
-  it.todo("equal object() ?");
 });
